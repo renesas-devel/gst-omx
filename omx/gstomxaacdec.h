f109a467062ea0b351f2ef7923cb9096032c6d56
Thao Luong <thao.luong.uw@rvc.renesas.com>
2014-11-12 14:16:03 +0700

    omx: Update Copyright for omxh264dec and omxaacdec
    Add REL copyright to these files
    
    refs #59333
    Signed-off-by: Thao Luong <thao.luong.uw@rvc.renesas.com>

--- omx/gstomxaacdec.h
+++ omx/gstomxaacdec.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2011, Hewlett-Packard Development Company, L.P.
+ * Copyright (C) 2014, Renesas Electronics Corporation
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
