f4a1f008c262b7c1938a526a5870f95e32cfe68c
Hung Tran <hung.tran.jy@rvc.renesas.com>
2015-01-15 14:54:07 +0700

    Update the Copyright year
    Signed-off-by: Hung Tran <hung.tran.jy@rvc.renesas.com>

--- omx/gstomxvideodec.c
+++ omx/gstomxvideodec.c
@@ -3,7 +3,7 @@
  *   Author: Sebastian Dröge <sebastian.droege@collabora.co.uk>, Collabora Ltd.
  * Copyright (C) 2013, Collabora Ltd.
  *   Author: Sebastian Dröge <sebastian.droege@collabora.co.uk>
- * Copyright (C) 2014, Renesas Electronics Corporation
+ * Copyright (C) 2014-2015, Renesas Electronics Corporation
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
